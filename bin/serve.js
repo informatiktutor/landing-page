778f318c5a2640e8e8fee63405ea0dee4ba440d1
Jonas van den Berg <jonas@informatiktutor.de>
2021-09-20 16:51:39 +0200

    Add script for running production version

--- bin/serve.js
+++ bin/serve.js
@@ -3,5 +3,5 @@ var express = require('express');
 var compression = require('compression');
 var app = express();
 app.use(compression());
-app.use("/", express.static(__dirname + '/docs'));
+app.use("/", express.static(__dirname + '/../docs'));
 app.listen(3000);
